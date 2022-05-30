87cb3c4ec1ea79a37ef1bcf0f5b3fffe781dd143
Jason Fritsche <fritsche.jason@gmail.com>
2022-05-30 16:35:06 -0500

    added login and signup components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,6 @@
 <h1 class="text-3xl font-bold underline text-rose-400">Hello world!</h1>
+
+<div>
+	have an account? <a href="/login">Log in</a>
+	Need an account? <a href="/signup">Sign up</a>
+</div>
